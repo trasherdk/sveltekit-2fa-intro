06cdace32a26d8a26d1866014467f12cb2fc3bff
trasherdk <trasherdk@gmail.com>
2023-08-25 10:18:29 +0200

    Move db to ignored subdir `data` - resolve a type warning

--- src/lib/util/database.js
+++ src/lib/util/database.js
@@ -3,9 +3,9 @@ import sqlite3 from 'sqlite3';
 /**
  * @returns {Promise<sqlite3.Database>}
  */
-export async function getDatabase() {
+export async function getDatabase () {
 	return new Promise((resolve, reject) => {
-		const db = new sqlite3.Database('db.sqlite', (err) => {
+		const db = new sqlite3.Database('/data/db.sqlite', (err) => {
 			if (err) return reject(err);
 			resolve(db);
 		});
@@ -18,7 +18,7 @@ export async function getDatabase() {
  * @param {Record<string, any>} params
  * @returns {Promise<sqlite3.RunResult>}
  */
-export async function runQuery(query, params = {}) {
+export async function runQuery (query, params = {}) {
 	const db = await getDatabase();
 	return new Promise((resolve, reject) => {
 		db.run(query, params, function (err) {
@@ -30,12 +30,15 @@ export async function runQuery(query, params = {}) {
 
 /**
  *
- * @param {string} userId
+ * @param {string | undefined} userId
  * @returns
  */
-export async function getUser(userId) {
+export async function getUser (userId) {
 	const db = await getDatabase();
 	return new Promise((resolve, reject) => {
+		if (!userId) {
+			reject(new Error('getUser: Missing userId'))
+		}
 		db.get('SELECT * FROM users WHERE id = ?', [userId], (err, row) => {
 			if (err) return reject(err);
 			resolve(row);
